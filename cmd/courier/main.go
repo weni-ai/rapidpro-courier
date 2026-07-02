e3f8ad3a5bca95ee9a287fdb26f5ee5000668399
lucaslinhares <lucaslinhares027@gmail.com>
2026-07-02 17:44:26 -0300

    Add turn io channel type

--- cmd/courier/main.go
+++ cmd/courier/main.go
@@ -62,6 +62,7 @@ import (
 	_ "github.com/nyaruka/courier/handlers/telegram"
 	_ "github.com/nyaruka/courier/handlers/telesom"
 	_ "github.com/nyaruka/courier/handlers/thinq"
+	_ "github.com/nyaruka/courier/handlers/turn"
 	_ "github.com/nyaruka/courier/handlers/twiml"
 	_ "github.com/nyaruka/courier/handlers/twitter"
 	_ "github.com/nyaruka/courier/handlers/viber"
